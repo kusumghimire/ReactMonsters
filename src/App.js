aaeb165653d7213588ab3c7338b6ee5e20a16f4c
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-01 14:20:12 +0545

    fetch is added which make an API url

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        // {
+        //   name: 'Franken',
+        //   id: 'asc1'
+        // },
+        // {
+        //   name: 'Dracula',
+        //   id: 'asc2'
+        // },
+        // {
+        //   name: 'Zombie',
+        //   id: 'asc13'
+        // }
+      ]
+    };
+  }
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users').then(response =>response.json())
+  .then(users=>this.setState({monsters:users}));
+}
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster =>
+            <h1 key={monster.id}>{monster.name}</h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
