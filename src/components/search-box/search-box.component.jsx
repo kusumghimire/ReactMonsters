cc97aa362fc87692fb065f7f64fb32f7e6188b24
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-02 17:18:10 +0545

    card.styles.css is created

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox =props =>(
+    <input className='search-box' type='search' placeholder='search monsters'
+ onChange={props.onSearchChange}/>
+)
