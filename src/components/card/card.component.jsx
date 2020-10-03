0dfaeb2cf660e75738fbbe699dd0f14d74c28be5
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-03 14:35:09 +0545

    image with props.monster.id is added

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = props=>(
 <div className='card-container'>
- <img src="" alt=""/>
+ <img src={'https://robohash.org/${props.monster.id}?set=set2&size=180*180'} alt="monster"/>
  <h2>{props.monster.name}</h2>
  <p>{props.monster.email}</p>
 </div>
