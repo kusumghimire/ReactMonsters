bd0340a676f970c6a78933c870b1c37061d76fd4
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-03 18:05:49 +0545

    Updates

---
+++ precache-manifest.0981b6945dd728b6f02ca12a2760a908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324e79077fa66470512ca8697e8e8443",
+    "url": "/ReactMonsters/index.html"
+  },
+  {
+    "revision": "84ff7453cd6d80b793e6",
+    "url": "/ReactMonsters/static/css/main.99e3b631.chunk.css"
+  },
+  {
+    "revision": "1c2fa360610e81c64fff",
+    "url": "/ReactMonsters/static/js/2.cf2353fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMonsters/static/js/2.cf2353fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ff7453cd6d80b793e6",
+    "url": "/ReactMonsters/static/js/main.0f8fb656.chunk.js"
+  },
+  {
+    "revision": "028cf28090b41d0d8ff9",
+    "url": "/ReactMonsters/static/js/runtime-main.b81325ef.js"
+  }
+]);
